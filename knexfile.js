e1d2d2d62bcaf22dab015cfa50531462fbc2e406
david-chua <dp_chua@hotmail.com>
2019-04-20 23:14:57 -0400

    answers MD and knexfile configured

--- knexfile.js
+++ knexfile.js
@@ -5,40 +5,28 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/games.db3'
+    },
+    useNullAsDefault: true,
+    migrations:{
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds',
     }
   },
 
-  staging: {
-    client: 'postgresql',
+  testing: {
+    client: 'sqlite3',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      filename: './data/gamesTest.db3'
     },
-    pool: {
-      min: 2,
-      max: 10
+    useNullAsDefault: true,
+    migrations:{
+      directory: './data/migrations'
     },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+    seeds: {
+      directory: './data/seeds',
     }
   }
-
 };
